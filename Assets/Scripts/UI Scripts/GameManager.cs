340dc4f172ab1992ae89638d7c7cd2702a20623f
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2020-02-22 18:15:17 +0200

    Added Game Manager and menu functionalities

---
+++ Assets/Scripts/UI Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    { 
+        Scene scene = SceneManager.GetActiveScene();
+    }
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
