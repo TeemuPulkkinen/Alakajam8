ba32b91fd2db62350967731d2f5979fe82e49397
Satu Remes <r3messa@gmail.com>
2020-02-22 13:22:41 +0200

    Väli enemy scripti committi

---
+++ Assets/Scripts/Enemy Scripts/EnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float rotate_Speed = 50f;
+
+    public bool canShoot;
+    public bool canRotate;
+    private bool canMove = true;
+
+    public float bound_Y = -11f;
+
+    public Transform attack_Point;
+    public GameObject enemyBullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
