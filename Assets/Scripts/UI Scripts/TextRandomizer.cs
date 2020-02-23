0f68aa40779028d504ceca583a2f60ef5561cc06
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2020-02-23 15:11:20 +0200

    Began text randomizer script.

---
+++ Assets/Scripts/UI Scripts/TextRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextRandomizer : MonoBehaviour
+{
+    GameObject gameObject;
+    Text paperText;
+    
+    private void Awake()
+    {
+        this.gameObject;
+    }
+
+    void RandomizeText()
+    {
+
+    }
+}
